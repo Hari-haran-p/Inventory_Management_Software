68a22779e4c2d5e5289bc594aafc802b33e996a4
raj-kavin <ragavkavinraj@gmail.com>
2024-04-30 14:03:11 +0530

    Transfer and Scrap Updated

--- server/database/db.js
+++ server/database/db.js
@@ -3,16 +3,16 @@ const dotenv = require("dotenv").config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  // host: '10.30.10.41',
-  // user: 'lab',
-  // password: 'Spl@765',
-  // database: 'LAB',
-  // port: 3306
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '10.30.10.41',
+  user: 'lab',
+  password: 'Spl@765',
   database: 'LAB',
   port: 3306
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
+  // database: 'LAB',
+  // port: 3306
 });
 
 module.exports = {
